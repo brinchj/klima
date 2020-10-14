3bb18b1c47e42d1eb6babf502b48a0b7849051ca
Johan Brinch <johan@pleo.io>
2020-10-14 15:37:14 +0200

    Attempt a simplification

--- src/dst/mod.rs
+++ src/dst/mod.rs
@@ -1,11 +1,44 @@
 mod models;
 
+use im;
+
 use models::data::DatasetContainer;
 use models::metadata::{Metadata, Value, Variable};
 use std::collections::BTreeMap;
 
 pub struct VarKey(String);
 pub struct ValKey(String);
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct DataPoint {
+    value: i64,
+    tags: im::OrdMap<String, String>
+}
+
+impl DataPoint {
+    fn join_dimensions_and_data(
+        dimensions: &[(&str, Vec<&str>)],
+        tags: im::OrdMap<String, String>,
+        data: &[i64],
+    ) -> im::Vector<DataPoint> {
+        match dimensions.split_first() {
+            None => im::Vector::unit(DataPoint{tags, value: data[0]}),
+            Some(((name, variants), dimensions)) => {
+                variants.iter().map(|variant|
+                    tags.update(name.to_string(), variant.to_string())
+                ).fold(im::Vector::new(), |res, tags| {
+                    &res + &DataPoint::join_dimensions_and_data(dimensions, tags, &data[res.len()..])
+                })
+            }
+        }
+    }
+
+    fn from_dimensions_and_data(dimensions: &[(&str, Vec<&str>)], data: &[i64]) -> Vec<DataPoint> {
+        return DataPoint::join_dimensions_and_data(dimensions, im::OrdMap::new(), data)
+            .into_iter()
+            .collect()
+    }
+}
 
 fn variable<'a>(metadata: &'a Metadata, var: &VarKey) -> Option<&'a Variable> {
     metadata.variables.iter().find(|v| v.id == var.0)
@@ -15,37 +48,19 @@ fn variable_code<'a>(metadata: &'a Metadata, var: &VarKey, val: &ValKey) -> Opti
     variable(metadata, var).and_then(|v| v.values.iter().find(|v| v.text == val.0))
 }
 
-fn join_dimensions_and_data(
-    dimensions: &[(&str, Vec<&str>)],
-    context: &mut BTreeMap<String, String>,
-    data: &[i64],
-) -> Vec<(BTreeMap<String, String>, i64)> {
-    return match dimensions.split_first() {
-        None => vec![(context.clone(), data[0])],
-        Some(((name, variants), tail)) => {
-            let mut res = vec![];
-            for variant in variants {
-                context.insert(name.to_string(), variant.to_string());
-                res.append(&mut join_dimensions_and_data(
-                    tail,
-                    context,
-                    &data[res.len()..],
-                ))
-            }
-            res
-        }
-    };
-}
+
+
+
 
 #[cfg(test)]
 mod tests {
-    use super::join_dimensions_and_data;
+    use super::DataPoint;
     use super::DatasetContainer;
     use super::Metadata;
     use std::collections::BTreeMap;
 
     #[test]
-    fn test_join_dimensions_and_data() {
+    fn test_from_dimensions_and_data() {
         let dimensions = vec![
             ("DRIV", vec!["Benzin", "Diesel", "El"]),
             ("EJER", vec!["Privat", "Erhverv"]),
@@ -53,7 +68,7 @@ mod tests {
             ("Tid", vec!["2018", "2019", "2020"]),
         ];
 
-        let mut m: Vec<(BTreeMap<String, String>, i64)> = vec![];
+        let mut m: Vec<DataPoint> = vec![];
 
         let mut values = vec![];
         for driv in &dimensions[0].1 {
@@ -62,26 +77,24 @@ mod tests {
                     for tid in &dimensions[3].1 {
                         let n = values.len() as i64;
                         values.push(n);
-                        m.push((
-                            vec![
+                        m.push(DataPoint {
+                            tags: vec![
                                 ("DRIV".to_string(), driv.to_string()),
                                 ("EJER".to_string(), ejer.to_string()),
                                 ("Data".to_string(), data.to_string()),
                                 ("Tid".to_string(), tid.to_string()),
                             ]
-                            .into_iter()
-                            .collect(),
-                            n,
-                        ));
+                                .into_iter()
+                                .collect(),
+                            value: n,
+                        });
                     }
                 }
             }
         }
 
-        let mut context = BTreeMap::new();
         assert_eq!(
-            join_dimensions_and_data(&dimensions, &mut context, &values),
-            m
+            DataPoint::from_dimensions_and_data(&dimensions, &values), m
         );
     }
 
